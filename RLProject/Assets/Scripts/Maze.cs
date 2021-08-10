23cc7f6bb58eade961d1e9ce59d435fba784cdcc
nadavsrb <69578111+nadavsrb@users.noreply.github.com>
2021-08-10 21:04:20 +0300

    starting the maze

---
+++ RLProject/Assets/Scripts/Maze.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Maze : MonoBehaviour
+{
+    public int HEIGHT = 0;
+    public int WIDTH = 0;
+    
+
+    private InnerMazeNode[,] mazeNodes;
+
+    [SerializeField] GameObject prefabMazeNode;
+
+    private class InnerMazeNode
+    {
+        private Object mazeNode;
+        private int row;
+        private int col;
+
+        InnerMazeNode(int row, int col, Object mazeNode)
+        {
+            this.row = row;
+            this.col = col;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mazeNodes = new InnerMazeNode[HEIGHT,WIDTH];
+
+        for(int row = 0; row < HEIGHT; ++row)
+        {
+            for(int col = 0; col < WIDTH; ++col)
+            {
+                var mazeNode = Instantiate(prefabMazeNode);
+
+                var transform = mazeNode.GetComponent<Transform>();
+
+                transform.SetParent(GetComponent<Transform>());
+
+                transform.position = new Vector3(((float)-WIDTH) / 2 + col + 0.5f, (float)HEIGHT / 2 - row - 0.5f);
+            }
+        }
+
+        MazeNode.setIsChangeable(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ RLProject/Assets/Scripts/MazeNode.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeNode : MonoBehaviour
+{
+    public delegate void changed();
+    public event changed notify;
+
+    private SpriteRenderer sr;
+    private readonly Color UNBLOCKED_COLOR = Color.black;
+    private readonly Color BLOCKED_COLOR = Color.red;
+    private static bool isChangeable = false;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        sr.color = UNBLOCKED_COLOR;
+    }
+
+    private void OnMouseDown()
+    {
+        if (!isChangeable) return;
+
+        sr.color = (sr.color == UNBLOCKED_COLOR) ? BLOCKED_COLOR : UNBLOCKED_COLOR;
+
+        notify();
+    }
+
+    public static void setIsChangeable(bool state)
+    {
+        isChangeable = state;
+    }
+}
