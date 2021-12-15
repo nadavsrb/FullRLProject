312776e794f501c6109a4d24a568ac5a68c1da2b
nadavsrb <69578111+nadavsrb@users.noreply.github.com>
2021-12-15 19:00:24 +0200

    Adding transitions

---
+++ RLProject/Assets/Scripts/ButtonSceneTransition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSceneTransition : MonoBehaviour
+{
+    public TransformLoader transformLoader;
+    public string sceneToLoad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(delegate()
+        {
+            transformLoader.LoadSceneByName(sceneToLoad);
+        });
+    }
+
+}
---
+++ RLProject/Assets/Scripts/TransformLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransformLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    public void LoadSceneByName(string SceneName)
+    {
+        StartCoroutine(LoadScene(SceneName));
+    }
+
+    private IEnumerator LoadScene(string SceneName)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(SceneName);
+    }
+}
